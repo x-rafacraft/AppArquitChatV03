7756d0b32d734d0fe76d916259ae38cdc6a3504d
x-rafacraft <rafaelreymundoserpa@gmail.com>
2024-04-20 08:57:51 -0500

    Initial commit

---
+++ app/src/main/java/com/rafacraft/apparquitchatv03/OpcionesLoginActivity.kt
@@ -0,0 +1,11 @@
+package com.rafacraft.apparquitchatv03
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OpcionesLoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_opciones_login)
+    }
+}
